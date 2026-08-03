6eee0235109e1d4640499e98870eeff781afa293
Shahd <shswe77@icloud.com>
2026-08-03 16:59:36 +0300

    Update marine location selection and dashboard improvements

---
+++ Sea/wwwroot/js/marine-location.js
@@ -0,0 +1,91 @@
+﻿function loadMarineLocation(callback) {
+
+
+    navigator.geolocation.getCurrentPosition(
+
+        function (position) {
+
+
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+
+
+            let saved = sessionStorage.getItem("marineLocation");
+
+
+            if (saved) {
+
+                let location = JSON.parse(saved);
+
+                callback(
+                    location.lat,
+                    location.lon,
+                    location.name,
+                    true
+                );
+
+            }
+            else {
+
+
+                // الحصول على اسم المدينة من الإحداثيات
+                fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`)
+                    .then(response => response.json())
+                    .then(data => {
+
+
+                        let address = data.address;
+
+
+                        let city =
+                            address.city ||
+                            address.town ||
+                            address.village ||
+                            address.state ||
+                            "Unknown Location";
+
+
+                        callback(
+                            lat,
+                            lon,
+                            city,
+                            false
+                        );
+
+
+                    })
+                    .catch(() => {
+
+
+                        callback(
+                            lat,
+                            lon,
+                            "Unknown Location",
+                            false
+                        );
+
+
+                    });
+
+            }
+
+
+        },
+
+
+        function () {
+
+
+            callback(
+                null,
+                null,
+                "Location unavailable",
+                false
+            );
+
+
+        }
+
+    );
+
+}
